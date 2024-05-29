b4b0f30cdf4f5871e3c1e0de18bdf6263747b681
Jose0272002 <135110380+Jose0272002@users.noreply.github.com>
2024-05-29 11:35:15 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule,HomeComponent],
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
